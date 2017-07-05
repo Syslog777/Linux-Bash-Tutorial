f3b040a045a5f37bbdcf0c5507f02bb37abcd5bc
BitCoder <tcodex250@gmail.com>
2017-07-05 12:03:08 -0500

    Update Install_Linux_Tools.sh

--- Install_Linux_Tools.sh
+++ Install_Linux_Tools.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #Installs all programs in a semi-automated manner.
+#Please replace password with your admin password before running this script.
 #Since this script must be run from the terminal, type ./Install_Linux_Tools.sh to run.
 myarray=(chkrootkit etherape nikto ettercap-graphical rkhunter
 p0f nmap aircrackng netdiscover reaver nbtscan macchanger synaptic
 firehol htop git get-config nano apt dpkg logkeys denyhosts tiger
 lynis away xtrlock clamtk zenmap lua5.1 sshd dsniff yum mate-terminal
-net)
+samba-common-bin)
 
 echo 'password' | sudo -S su
 
