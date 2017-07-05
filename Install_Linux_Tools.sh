e9678a293838d4e7eee3dbcc71476b288a1dc417
BitCoder <tcodex250@gmail.com>
2017-07-05 12:00:22 -0500

    Update Install_Linux_Tools.sh

--- Install_Linux_Tools.sh
+++ Install_Linux_Tools.sh
@@ -4,7 +4,8 @@
 myarray=(chkrootkit etherape nikto ettercap-graphical rkhunter
 p0f nmap aircrackng netdiscover reaver nbtscan macchanger synaptic
 firehol htop git get-config nano apt dpkg logkeys denyhosts tiger
-lynis away xtrlock clamtk zenmap lua5.1 sshd dsniff yum mate-terminal)
+lynis away xtrlock clamtk zenmap lua5.1 sshd dsniff yum mate-terminal
+net)
 
 echo 'password' | sudo -S su
 
