7e004bc3814447d2641b53ec0516e4155b62adfe
BitCoder <tcodex250@gmail.com>
2017-09-14 13:13:19 -0500

    Update Android_Studio_Script.sh

--- Android_Studio_Script.sh
+++ Android_Studio_Script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #I didnt have a way to open androidd studio by a simple click, so I wrote this script.
 echo "Attempting to start Android studio up"
-
-/usr/local/android-studio/bin/studio.sh
+echo 'password' | sudo -S /usr/local/android-studio/bin/studio.sh
