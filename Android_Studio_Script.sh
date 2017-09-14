677fc1d7d21837ba77004e18eb8c589d9568fd74
BitCoder <tcodex250@gmail.com>
2017-09-14 13:19:50 -0500

    Update Android_Studio_Script.sh

--- Android_Studio_Script.sh
+++ Android_Studio_Script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #I didnt have a way to open androidd studio by a simple click, so I wrote this script.
+#If you get an error such as "bash: permission denied", type chmod u+x Android_Studio_Script.sh
 echo "Attempting to start Android studio up"
 echo 'password' | sudo -S /usr/local/android-studio/bin/studio.sh
