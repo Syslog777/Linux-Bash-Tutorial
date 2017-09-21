0822d7a57a9e27159370dc5577b6a2aff4a2f483
BitCoder <tcodex250@gmail.com>
2017-09-20 23:01:27 -0400

    Update root-terminal-v3.sh

--- root-terminal-v3.sh
+++ root-terminal-v3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 #If expect is not installed, install it.
 #Remember to either put this in your /bin directory, or in your path.
-#Reference: https://stackoverflow.com/questions/233217/how-to-pass-the-password-to-su-sudo-ssh-without-overriding-the-tty
+#Reference: https://unix.stackexchange.com/questions/52344/shell-script-to-become-root
 spawn sudo su -
 expect "Password: "
 send "thepassword\r"
