396afb6adf62f52f4db374b18456e33e8ea3aba9
BitCoder <tcodex250@gmail.com>
2017-09-21 18:07:15 -0400

    Update Install_Linux_Tools.sh

--- Install_Linux_Tools.sh
+++ Install_Linux_Tools.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Installs all programs in a semi-automated manner.
-#Please replace password with your admin password before running this script.
 #Since this script must be run from the terminal, type ./Install_Linux_Tools.sh to run.
 myarray=(chkrootkit etherape nikto ettercap-graphical rkhunter
 p0f nmap aircrackng netdiscover reaver nbtscan macchanger synaptic
