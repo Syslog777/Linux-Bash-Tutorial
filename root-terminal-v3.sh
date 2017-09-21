8e31d7f6480cba2c471b6e2ebf8f3fff27911ff9
BitCoder <tcodex250@gmail.com>
2017-09-20 22:59:58 -0400

    Update root-terminal-v3.sh

--- root-terminal-v3.sh
+++ root-terminal-v3.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/expect
 #If expect is not installed, install it.
+#Remember to either put this in your /bin directory, or in your path.
 spawn sudo su -
 expect "Password: "
 send "thepassword\r"
