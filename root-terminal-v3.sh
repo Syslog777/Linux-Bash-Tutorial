e963038c743c255e58ce112818a98ddd420716c6
BitCoder <18704463+BitCoder444@users.noreply.github.com>
2017-10-01 23:15:10 -0400

    Update root-terminal-v3.sh

--- root-terminal-v3.sh
+++ root-terminal-v3.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/expect
-#If expect is not installed, install it.
+#If "expect" is not installed, install it by running 'sudo apt-get install expect'
 #Remember to either put this in your /bin directory, or in your path.
 #Reference: https://unix.stackexchange.com/questions/52344/shell-script-to-become-root
-spawn sudo su -
+spawn su -l root -
 expect "Password: "
-send "thepassword\r"
+send "JK6MousePadFloorboardHat$$\r"
 interact
