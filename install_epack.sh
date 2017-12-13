ae3787e86655e8c62c599ce07d9e922600523198
Syslog777 <18704463+Syslog777@users.noreply.github.com>
2017-12-13 02:21:15 -0600

    Update install_epack.sh

--- install_epack.sh
+++ install_epack.sh
@@ -6,15 +6,11 @@ myarray=(chkrootkit etherape nikto ettercap-graphical rkhunter
 p0f nmap aircrackng netdiscover reaver nbtscan macchanger synaptic
 firehol htop git get-config nano apt dpkg logkeys denyhosts tiger
 lynis away xtrlock clamtk zenmap lua5.1 sshd dsniff yum mate-terminal
-samba-common-bin aircrack-ng iprange cmake linuxlogo figlet)
-printf "
-Produced by
-    ____  _ __  ______          __         
-   / __ )(_) /_/ ____/___  ____/ /__  _____
-  / __  / / __/ /   / __ \/ __  / _ \/ ___/
- / /_/ / / /_/ /___/ /_/ / /_/ /  __/ /    
-/_____/_/\__/\____/\____/\__,_/\___/_/ 
-"
+samba-common-bin aircrack-ng iprange cmake linuxlogo figlet lice)
+
+print '
+lice: a liscence output program'
+
 sudo su
 
 for program in "${myarray[@]}" 
